4468cc62beb338de757ac4508ab0b24a8586dec9
LuisGustavoRubi <161526218+LuisGustavoRubi@users.noreply.github.com>
2025-03-06 15:09:22 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
-import { defineConfig } from 'vite'
-
+import { defineConfig } from 'vite';
+import htmlPurge from 'vite-plugin-purgecss';
 export default defineConfig({
-    appType: 'mpa'
+    appType: 'mpa',
+    base: process.env.DEPLOY_BASE_URL,
+    plugins: [
+        htmlPurge()
+    ]
 });
